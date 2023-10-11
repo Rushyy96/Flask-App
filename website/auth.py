ca5c9e81f3bcdddca7fa53393dc8c4002bccc56e
jack rushton <jackrushton96@hotmail.co.uk>
2023-10-11 11:21:13 +0100

    routing

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('login')
+@auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 
 @auth.route('/logout')
@@ -13,6 +13,6 @@ def logout():
     return "<p>logout</p>"
 
 
-@auth.route('sign-up')
+@auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("signup.html")
